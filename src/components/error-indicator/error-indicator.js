b8173ab1fbe1f20321b07264f5b4f2b61746ed49
vl-c <vladimir.chirgin@gmail.com>
2019-12-03 06:02:35 +0300

    added error handling and resize spinner

---
+++ src/components/error-indicator/error-indicator.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import icon from './death-star-icon.png';
+import './error-indicator.css';
+
+const ErrorIndicator = () => {
+    return (
+        <div className="error-indicator">
+            <img src={icon} alt="error-icon" />
+            <span className="boom">BOOM!</span>
+            <span>
+                something has gone terribly wrong
+            </span>
+            <span>
+                (but we already sent droids to fix it)
+            </span>
+        </div>
+    );
+}
+
+export default ErrorIndicator;
