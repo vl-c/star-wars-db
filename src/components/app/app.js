0dd25e76a1af51742f4283139d24cf1533d61dea
vl-c <vladimir.chirgin@gmail.com>
2019-12-07 20:05:15 +0300

    add error boundry

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,10 +1,12 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Header from '../header';
 import ItemList from '../item-list';
 import PersonDetails from '../person-details';
 import RandomPlanet from '../random-planet';
 import './app.css';
 import SwapiService from '../../services/swapi-service';
+import Row from '../row';
+import ErrorBoundry from '../error-boundry';
 
 export default class App extends Component {
 
@@ -21,21 +23,48 @@ export default class App extends Component {
     }
 
     render() {
-        return(
+        const itemList = (
+            <ItemList
+                onPersonSelected={this.onPersonSelected}
+                service={this.swapiService.getAllPeople}>
+                {(i) => (
+                    `${i.name} (${i.birthYear})`
+                )}
+            </ItemList>
+
+        );
+
+        const planetsList = (
+            <ItemList
+                onPersonSelected={this.onPersonSelected}
+                service={this.swapiService.getAllPlanets}
+                renderFunction={({ name, diameter }) => {
+                    return `${name} (${diameter})`
+                }} />
+        );
+
+        const starshipsList = (
+            <ItemList
+                onPersonSelected={this.onPersonSelected}
+                service={this.swapiService.getAllStarships}
+                renderFunction={({ name, model }) => {
+                    return `${name} (${model})`
+                }} />
+        );
+
+        const personDetails = (
+            <PersonDetails personId={this.state.selectedPerson} />
+        );
+
+        return (
             <div className="container">
                 <Header />
                 <RandomPlanet />
-
-                <div className="row mb2">
-                    <div className="col-md-6">
-                        <ItemList
-                            onPersonSelected={this.onPersonSelected}
-                            service={this.swapiService.getAllPeople}/>
-                    </div>
-                    <div className="col-md-6">
-                        <PersonDetails personId={this.state.selectedPerson}/>
-                    </div>
-                </div>
+                <ErrorBoundry>
+                    <Row left={itemList} right={personDetails} />
+                </ErrorBoundry>
+                {/* <Row left={planetsList} right={personDetails} />
+                <Row left={starshipsList} right={personDetails} /> */}
             </div>
         );
     }
--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './item-list.css';
 import Spinner from '../spinner';
 
@@ -9,7 +9,7 @@ export default class ItemList extends Component {
     }
 
     componentDidMount() {
-        const {service} = this.props;
+        const { service } = this.props;
         service()
             .then((items) => {
                 this.setState({
@@ -19,27 +19,29 @@ export default class ItemList extends Component {
     }
 
     renderItems(arr) {
-        return arr.map(({id, name}) => {
+        return arr.map((item) => {
+            const { id } = item;
+            const label = this.props.children(item);
             return (
                 <li className="list-group-item"
                     onClick={() => this.props.onPersonSelected(id)}
                     key={id}>
-                    {name}
+                    {label}
                 </li>
             )
         });
     }
 
     render() {
-        const {items} = this.state;
+        const { items } = this.state;
         if (!items) {
             return <Spinner />;
         }
         const renderedItems = this.renderItems(items);
-        return(
+        return (
             <ul className="item-list list-group">
                 {renderedItems}
-          </ul>
+            </ul>
         );
     }
 }
